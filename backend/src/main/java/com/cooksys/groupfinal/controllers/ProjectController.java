217d0962da056dde5c99c58e6863d02168049002
Rickey Weems <weems505@gmail.com>
2024-03-25 14:01:38 -0400

    Added crud features

--- backend/src/main/java/com/cooksys/groupfinal/controllers/ProjectController.java
+++ backend/src/main/java/com/cooksys/groupfinal/controllers/ProjectController.java
@@ -1,11 +1,14 @@
 package com.cooksys.groupfinal.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cooksys.groupfinal.dtos.ProjectDto;
+import org.springframework.web.bind.annotation.*;
 
 import com.cooksys.groupfinal.services.ProjectService;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/projects")
@@ -13,5 +16,26 @@ import lombok.RequiredArgsConstructor;
 public class ProjectController {
 	
 	private final ProjectService projectService;
+
+
+	@PostMapping()
+	public ProjectDto createProject(@RequestBody ProjectDto projectDto){
+		return projectService.createProject(projectDto);
+	}
+
+	@GetMapping()
+	public Set<ProjectDto> getAllProjects(){
+		return projectService.getAllProjects();
+	}
+	@GetMapping("{teamId}")
+	public Set<ProjectDto> getProjectsByTeam(@PathVariable long teamId){
+		return projectService.getProjectsByTeamId(teamId);
+	}
+
+	@PatchMapping("/project/{projectId}")
+	public ProjectDto updateProject(@PathVariable long projectId, @RequestBody ProjectDto projectDto){
+		return projectService.updateProject(projectId, projectDto);
+	}
+
 
 }
