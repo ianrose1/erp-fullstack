d44a22f22fcbd7e5baec3750903e32f99d07e80f
Gregory Herringer <greg.herringer@gmail.com>
2024-03-25 15:45:54 -0700

    implements announcement service

---
+++ frontend/src/app/services/announcements.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+import { BehaviorSubject } from 'rxjs';
+import Announcement from '../interfaces/announcement';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnnouncementsService {
+
+  private announcementsSubject = new BehaviorSubject<Announcement[]>([]);
+  announcements$ = this.announcementsSubject.asObservable();
+
+  constructor() { }
+
+  async fetchAnnouncements(companyId: number) {
+    try {
+      const response = await axios.get(`/company/${companyId}/announcements`);
+      console.log("Announcements Response Data: ", response.data);
+      this.announcementsSubject.next(response.data);
+    }
+    catch (error) {
+      console.error("Error fetching announcements:", error);
+    }
+  }
+
+
+}
