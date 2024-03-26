7febfdcadb3adc40c30ff21b65a633ceadf8e1ab
Rickey Weems <weems505@gmail.com>
2024-03-26 10:47:31 -0400

    Added user endpoints

--- backend/src/main/java/com/cooksys/groupfinal/controllers/UserController.java
+++ backend/src/main/java/com/cooksys/groupfinal/controllers/UserController.java
@@ -1,16 +1,15 @@
 package com.cooksys.groupfinal.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cooksys.groupfinal.dtos.UserRequestDto;
+import org.springframework.web.bind.annotation.*;
 
 import com.cooksys.groupfinal.dtos.CredentialsDto;
 import com.cooksys.groupfinal.dtos.FullUserDto;
 import com.cooksys.groupfinal.services.UserService;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/users")
@@ -23,6 +22,27 @@ public class UserController {
 	@CrossOrigin(origins="*")
     public FullUserDto login(@RequestBody CredentialsDto credentialsDto) {
         return userService.login(credentialsDto);
+    }
+
+
+    @GetMapping()
+    public Set<FullUserDto> getUsers(){
+        return userService.getUsers();
+    }
+
+    @GetMapping("{userId}")
+    public FullUserDto getUser(@PathVariable long userId){
+        return userService.getUser(userId);
+    }
+
+    @DeleteMapping("{userId}")
+    public FullUserDto deleteUser(@PathVariable long userId){
+        return userService.deleteUser(userId);
+    }
+
+    @PostMapping()
+    public FullUserDto createUser(@RequestBody UserRequestDto userRequestDto) {
+        return userService.createUser(userRequestDto);
     }
 
 }
