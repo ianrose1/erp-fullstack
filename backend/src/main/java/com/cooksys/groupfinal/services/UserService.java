364cff99ef5d185060f5e99c0b20745f82f4a55f
Rickey Weems <weems505@gmail.com>
2024-03-26 10:47:52 -0400

    Implemented endpoints

--- backend/src/main/java/com/cooksys/groupfinal/services/UserService.java
+++ backend/src/main/java/com/cooksys/groupfinal/services/UserService.java
@@ -2,10 +2,20 @@ package com.cooksys.groupfinal.services;
 
 import com.cooksys.groupfinal.dtos.CredentialsDto;
 import com.cooksys.groupfinal.dtos.FullUserDto;
+import com.cooksys.groupfinal.dtos.UserRequestDto;
+
+import java.util.Set;
 
 public interface UserService {
 
 	FullUserDto login(CredentialsDto credentialsDto);
 
-   
+
+    Set<FullUserDto> getUsers();
+
+    FullUserDto getUser(long userId);
+
+    FullUserDto deleteUser(long userId);
+
+    FullUserDto createUser(UserRequestDto userRequestDto);
 }
--- backend/src/main/java/com/cooksys/groupfinal/services/impl/UserServiceImpl.java
+++ backend/src/main/java/com/cooksys/groupfinal/services/impl/UserServiceImpl.java
@@ -1,7 +1,10 @@
 package com.cooksys.groupfinal.services.impl;
 
 import java.util.Optional;
+import java.util.Set;
 
+import com.cooksys.groupfinal.dtos.UserRequestDto;
+import com.cooksys.groupfinal.mappers.ProfileMapper;
 import org.springframework.stereotype.Service;
 
 import com.cooksys.groupfinal.dtos.CredentialsDto;
@@ -18,25 +21,28 @@ import com.cooksys.groupfinal.services.UserService;
 
 import lombok.RequiredArgsConstructor;
 
+import javax.swing.text.html.Option;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-	
-	private final UserRepository userRepository;
-  private final FullUserMapper fullUserMapper;
-	private final CredentialsMapper credentialsMapper;
-	
-	private User findUser(String username) {
+
+    private final UserRepository userRepository;
+    private final FullUserMapper fullUserMapper;
+    private final CredentialsMapper credentialsMapper;
+    private final ProfileMapper profileMapper;
+
+    private User findUser(String username) {
         Optional<User> user = userRepository.findByCredentialsUsernameAndActiveTrue(username);
         if (user.isEmpty()) {
             throw new NotFoundException("The username provided does not belong to an active user.");
         }
         return user.get();
     }
-	
-	@Override
-	public FullUserDto login(CredentialsDto credentialsDto) {
-		if (credentialsDto == null || credentialsDto.getUsername() == null || credentialsDto.getPassword() == null) {
+
+    @Override
+    public FullUserDto login(CredentialsDto credentialsDto) {
+        if (credentialsDto == null || credentialsDto.getUsername() == null || credentialsDto.getPassword() == null) {
             throw new BadRequestException("A username and password are required.");
         }
         Credentials credentialsToValidate = credentialsMapper.dtoToEntity(credentialsDto);
@@ -45,16 +51,51 @@ public class UserServiceImpl implements UserService {
             throw new NotAuthorizedException("The provided credentials are invalid.");
         }
         if (userToValidate.getStatus().equals("PENDING")) {
-        	userToValidate.setStatus("JOINED");
-        	userRepository.saveAndFlush(userToValidate);
+            userToValidate.setStatus("JOINED");
+            userRepository.saveAndFlush(userToValidate);
         }
         return fullUserMapper.entityToFullUserDto(userToValidate);
-	}
-	
-	
-	
-	
-	
-	
+    }
+
+    @Override
+    public Set<FullUserDto> getUsers() {
+        return fullUserMapper.entitiesToFullUserDtos(userRepository.findByActiveTrue());
+    }
+
+    @Override
+    public FullUserDto getUser(long userId) {
+        User user = userRepository.findByIdAndActiveTrue(userId);
+
+        if (user == null) {
+            throw new BadRequestException("No user found");
+        }
+
+        return fullUserMapper.entityToFullUserDto(user);
+    }
+
+    @Override
+    public FullUserDto deleteUser(long userId) {
+        User user = userRepository.findByIdAndActiveTrue(userId);
+        if (user == null) {
+            throw new BadRequestException("No user found");
+        }
+        user.setActive(false);
+        return fullUserMapper.entityToFullUserDto(userRepository.saveAndFlush(user));
+    }
+
+    @Override
+    public FullUserDto createUser(UserRequestDto userRequestDto) {
+        if (userRequestDto == null) {
+            throw new BadRequestException("User information cannot be null");
+        }
+
+        User userToCreate = fullUserMapper.requestDtoToEntity(userRequestDto);
+        userRepository.saveAndFlush(userToCreate);
+        userToCreate.setCredentials(credentialsMapper.dtoToEntity(userRequestDto.getCredentials()));
+        userToCreate.setProfile(profileMapper.dtoToEntity(userRequestDto.getProfile()));
+
+        return fullUserMapper.entityToFullUserDto(userToCreate);
+    }
+
 
 }
