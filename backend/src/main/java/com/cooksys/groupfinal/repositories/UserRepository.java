c1c48fe0e261ff8169f986670cbf990ddfb2bbdb
Rickey Weems <weems505@gmail.com>
2024-03-26 10:47:42 -0400

    Added derived queries

--- backend/src/main/java/com/cooksys/groupfinal/repositories/UserRepository.java
+++ backend/src/main/java/com/cooksys/groupfinal/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package com.cooksys.groupfinal.repositories;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,5 +14,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	
 	Optional<User> findByCredentialsUsernameAndActiveTrue(String username);
 
+	Set<User> findByActiveTrue();
+
+	User findByIdAndActiveTrue(Long id);
 
 }
