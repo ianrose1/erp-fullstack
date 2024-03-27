16c11c0c9e8615da97ae6996e95cd9c1bbefed31
Gregory Herringer <greg.herringer@gmail.com>
2024-03-26 21:20:12 -0700

    updates announcements page

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import Announcement from 'src/app/interfaces/announcement';
 import { AnnouncementsService } from 'src/app/services/announcements.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   announcements$: Observable<Announcement[]> = this.announcementService.announcementObservable();
 
-  constructor(private announcementService: AnnouncementsService) {}
+  constructor(private announcementService: AnnouncementsService, private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.announcementService.fetchAnnouncements();
+  }
 
 }
--- frontend/src/app/services/announcements.service.ts
+++ frontend/src/app/services/announcements.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import axios from 'axios';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, take } from 'rxjs';
 import Announcement from '../interfaces/announcement';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +12,15 @@ export class AnnouncementsService {
   private announcementsSubject = new BehaviorSubject<Announcement[]>([]);
   announcements$ = this.announcementsSubject.asObservable();
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   announcementObservable(){
     return this.announcementsSubject.asObservable();
   }
 
-  async fetchAnnouncements(companyId: number) {
+  async fetchAnnouncements() {
     try {
+      const companyId: number = this.userService.getCurrentCompanyId();
       const response = await axios.get(`http://localhost:8080/company/${companyId}/announcements`);
       console.log("Announcements Response Data: ", response.data);
       this.announcementsSubject.next(response.data);
