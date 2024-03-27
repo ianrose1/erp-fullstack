0a92c9d9b8b1297a9e2ebe6b99e90e774f54d9ef
Gregory Herringer <greg.herringer@gmail.com>
2024-03-26 18:46:50 -0700

    implements createNewAnnouncement post request and updates url to localhost java server

--- frontend/src/app/services/announcements.service.ts
+++ frontend/src/app/services/announcements.service.ts
@@ -19,12 +19,27 @@ export class AnnouncementsService {
 
   async fetchAnnouncements(companyId: number) {
     try {
-      const response = await axios.get(`/company/${companyId}/announcements`);
+      const response = await axios.get(`http://localhost:8080/company/${companyId}/announcements`);
       console.log("Announcements Response Data: ", response.data);
       this.announcementsSubject.next(response.data);
     }
     catch (error) {
       console.error("Error fetching announcements:", error);
+    }
+  }
+
+  // create new announcement needs 1) companyId and 2)userId
+  async createNewAnnouncement(companyId: number, userId: number, title: string, message: string) {
+    try {
+      const response = await axios.post(`http://localhost:8080/announcements/${userId}/${companyId}`, {
+        title,
+        message
+      });
+      console.log("Post New Announcement Response Data: ", response.data);
+      this.announcementsSubject.next(response.data);
+    }
+    catch (error) {
+      console.error("Error creating new announcement:", error);
     }
   }
 
